167dfb04c8892beee14997ac57e87f1fd4bb9b36
Dean <carmidean@gmail.com>
2020-01-28 15:59:13 +0200

    with checksum

--- part3/spoofer.c
+++ part3/spoofer.c
@@ -72,7 +72,34 @@ void send_packet( struct ipheader *ip){
 
 }
 
+unsigned short in_cksum (unsigned short *buf, int length)
+{
+   unsigned short *w = buf;
+   int nleft = length;
+   int sum = 0;
+   unsigned short temp=0;
 
+   /*
+    * The algorithm uses a 32 bit accumulator (sum), adds
+    * sequential 16 bit words to it, and at the end, folds back all 
+    * the carry bits from the top 16 bits into the lower 16 bits.
+    */
+   while (nleft > 1)  {
+       sum += *w++;
+       nleft -= 2;
+   }
+
+   /* treat the odd byte at the end, if any */
+   if (nleft == 1) {
+        *(u_char *)(&temp) = *(u_char *)w ;
+        sum += temp;
+   }
+
+   /* add back carry outs from top 16 bits to low 16 bits */
+   sum = (sum >> 16) + (sum & 0xffff);  // add hi 16 to low 16 
+   sum += (sum >> 16);                  // add carry 
+   return (unsigned short)(~sum);
+}
 
 int main (){
 
@@ -88,6 +115,13 @@ int main (){
     struct icmpheader *icmp = (struct icmpheader*)(buff + sizeof(struct ipheader));
     icmp->icmp_type = 8;                // ping request
    
+    //checksum
+    icmp->icmp_chksum = 0;
+    icmp->icmp_chksum = in_cksum((unsigned short *)icmp, 
+                                 sizeof(struct icmpheader));
+
+
+
     ////////// IP header config
     struct ipheader *ip = (struct ipheader *)(buff);
     ip->iph_len = htons(sizeof(struct ipheader) + sizeof(struct icmpheader));
