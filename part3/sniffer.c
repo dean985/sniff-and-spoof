9808a5248075d2f487b3cc61c1bf5d5846146f80
Dean <carmidean@gmail.com>
2020-01-28 14:00:38 +0200

    added print data

--- part3/sniffer.c
+++ part3/sniffer.c
@@ -74,6 +74,96 @@ struct tcpheader {
     u_short tcp_urp;                 /* urgent pointer */
 };
 
+void
+print_hex_ascii_line(const u_char *payload, int len, int offset)
+{
+
+	int i;
+	int gap;
+	const u_char *ch;
+
+	/* offset */
+	printf("%05d   ", offset);
+	
+	/* hex */
+	ch = payload;
+	for(i = 0; i < len; i++) {
+		printf("%02x ", *ch);
+		ch++;
+		/* print extra space after 8th byte for visual aid */
+		if (i == 7)
+			printf(" ");
+	}
+	/* print space to handle line less than 8 bytes */
+	if (len < 8)
+		printf(" ");
+	
+	/* fill hex gap with spaces if not full line */
+	if (len < 16) {
+		gap = 16 - len;
+		for (i = 0; i < gap; i++) {
+			printf("   ");
+		}
+	}
+	printf("   ");
+	
+	/* ascii (if printable) */
+	ch = payload;
+	for(i = 0; i < len; i++) {
+		if (isprint(*ch))
+			printf("%c", *ch);
+		else
+			printf(".");
+		ch++;
+	}
+
+	printf("\n");
+
+return;
+}
+
+void print_data(const u_char *payload, int len)
+{
+
+	int len_rem = len;
+	int line_width = 16;			/* number of bytes per line */
+	int line_len;
+	int offset = 0;					/* zero-based offset counter */
+	const u_char *ch = payload;
+
+	if (len <= 0)
+		return;
+
+	/* data fits on one line */
+	if (len <= line_width) {
+		print_hex_ascii_line(ch, len, offset);
+		return;
+	}
+
+	/* data spans multiple lines */
+	for ( ;; ) {
+		/* compute current line length */
+		line_len = line_width % len_rem;
+		/* print line */
+		print_hex_ascii_line(ch, line_len, offset);
+		/* compute total remaining */
+		len_rem = len_rem - line_len;
+		/* shift pointer to remaining bytes to print */
+		ch = ch + line_len;
+		/* add offset */
+		offset = offset + line_width;
+		/* check if we have line width chars or less */
+		if (len_rem <= line_width) {
+			/* print last line and get out */
+			print_hex_ascii_line(ch, len_rem, offset);
+			break;
+		}
+	}
+
+return;
+}
+
+
 void got_packet( u_char *args, const struct pcap_pkthdr *header,
                     const u_char *packet)
 {
@@ -94,6 +184,15 @@ void got_packet( u_char *args, const struct pcap_pkthdr *header,
         printf("   Protocol- TCP\n");
         printf("   Dest port: %d\n", (int)(ntohs(tcp->tcp_sport)));
         printf("   Src port : %d\n", (int)(ntohs(tcp->tcp_dport)));
+        if ((int)(ntohs(tcp->tcp_dport)) == 23 || (int)(ntohs(tcp->tcp_sport)) == 23){
+            int size_tcp = TH_OFF(tcp)*4;
+            int size_ethernet = sizeof(struct ethheader);
+            int size_ip = IP_HL(ip)*4;
+            int size_data = ntohs(ip->iph_len) - (size_ip + size_tcp);
+            const u_char* data = (unsigned char*)( packet + size_ethernet + size_ip +size_tcp );
+            printf(" Data size : %d bytes", size_data);
+            print_data(data, size_data);
+        }
       }
     }
     
